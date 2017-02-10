7e404aff30574f865bdf52b34ae83371b4ca8feb
Daniel Andrews <danielandrewsre@gmail.com>
2017-02-09 16:22:55 -0800

    Initial Commit
    Most things working, lots of UI changes to make and need to add other
    screens

--- MiniProject1/ViewController.swift
+++ MiniProject1/ViewController.swift
@@ -9,10 +9,188 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let members: [String] = ["Jessica Cherny", "Kevin Jiang", "Jared Gutierrez", "Kristin Ho", "Christine Munar", "Mudit Mittal", "Richard Hu", "Shaan Appel", "Edward Liu", "Wilbur Shi", "Young Lin", "Abhinav Koppu", "Abhishek Mangla", "Akkshay Khoslaa", "Ally Koo", "Andy Wang", "Aneesh Jindal", "Anisha Salunkhe", "Aparna Krishnan", "Ashwin Vaidyanathan", "Cody Hsieh", "Jeffrey Zhang", "Justin Kim", "Krishnan Rajiyah", "Lisa Lee", "Peter Schafhalter", "Sahil Lamba", "Sameer Suresh", "Sirjan Kafle", "Tarun Khasnavis", "Billy Lu", "Aayush Tyagi", "Ben Goldberg", "Candice Ye", "Eliot Han", "Emaan Hariri", "Jessica Chen", "Katharine Jiang", "Kedar Thakkar", "Leon Kwak", "Mohit Katyal", "Rochelle Shen", "Sayan Paul", "Sharie Wang", "Shreya Reddy", "Shubham Goenka", "Victor Sun", "Vidya Ravikumar"];
+
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var statsPicture: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var option1Label: UIButton!
+    @IBOutlet weak var option2Label: UIButton!
+    @IBOutlet weak var option3Label: UIButton!
+    @IBOutlet weak var option4Label: UIButton!
+    
+    var name: String = ""
+    var streak: Int = 0
+    var score: Int = 0
+    var timeLeft: Int = 5
+    var gameTimer: Timer = Timer()
+    
+    @IBAction func option1Button(_ sender: Any) {
+        submit(option1Label.currentTitle!, btnNumber: 1, btn: option1Label)
+    }
+    
+    @IBAction func option2Button(_ sender: Any) {
+        submit(option2Label.currentTitle!, btnNumber: 2, btn: option2Label)
+    }
+    
+    @IBAction func option3Button(_ sender: Any) {
+        submit(option3Label.currentTitle!, btnNumber: 3, btn: option3Label)
+    }
+    
+    @IBAction func option4Button(_ sender: Any) {
+        submit(option4Label.currentTitle!, btnNumber: 4, btn: option4Label)
+    }
+    
+    @IBAction func statsButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        
+    }
+    
+    func getName() -> String {
+        let size = members.count;
+        let randIndex = Int(arc4random_uniform(UInt32(size)))
+        return members[randIndex]
+    }
+    
+    
+    struct defaultsKeys {
+        static let longestStreak = "LongestStreak"
+        static let points = "Points"
+        static let name1 = "Name1"
+        static let name2 = "Name2"
+        static let name3 = "Name3"
+        static let bool1 = "Bool1"
+        static let bool2 = "Bool2"
+        static let bool3 = "Bool3"
+        static let currentStreak = "CurrentStreak"
+    }
+    
+    func storeValues(streak: Int, score: Int, correct: Bool, name: String) {
+        let defaults = UserDefaults.standard
+        
+        let longest = defaults.integer(forKey: defaultsKeys.longestStreak)
+        let name1 = defaults.string(forKey: defaultsKeys.name1)
+        let name2 = defaults.string(forKey: defaultsKeys.name2)
+        let bool1 = defaults.bool(forKey: defaultsKeys.bool1)
+        let bool2 = defaults.bool(forKey: defaultsKeys.bool2)
+        
+        defaults.setValue(streak, forKey: defaultsKeys.currentStreak)
+        if longest > streak {
+            defaults.setValue(streak, forKey: defaultsKeys.longestStreak)
+        }
+        
+        defaults.setValue(score, forKey: defaultsKeys.points)
+        
+        defaults.setValue(name2, forKey: defaultsKeys.name3)
+        defaults.setValue(name1, forKey: defaultsKeys.name2)
+        defaults.setValue(name, forKey: defaultsKeys.name1)
+        
+        defaults.setValue(bool2, forKey: defaultsKeys.bool3)
+        defaults.setValue(bool1, forKey: defaultsKeys.bool2)
+        defaults.setValue(correct, forKey: defaultsKeys.bool1)
+        
+        defaults.synchronize()
+    }
+    
+    func submit(_ guessedName: String?, btnNumber: Int, btn: UIButton?) {
+        gameTimer.invalidate()
+        
+        if guessedName != nil {
+            if guessedName == name {
+                //you got it right
+                streak += 1
+                score += 1
+                
+                //change button to green
+                btn!.backgroundColor = UIColor.green
+            } else {
+                //You got it wrong
+                streak = 0
+                
+                //change button to red
+                btn!.backgroundColor = UIColor.red
+            }
+        }
+        
+        //Store and replace values
+        storeValues(streak: streak, score: score, correct: guessedName == name, name: name)
+        
+        //move on to next question
+        waitASecond();
+    }
+    
+    func waitASecond() {
+        var timer = Timer()
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval:1, target: self, selector: #selector(setupGame), userInfo: nil, repeats: false)
+    }
+    
+    func getNames(used name: String, number n: Int) -> [String] {
+        var names: [String] = []
+        
+        while names.count < n {
+            let newName = getName()
+            
+            if newName != name && !names.contains(newName) {
+                names.append(newName)
+            }
+        }
+        
+        return names
+    }
+    
+    func setUpOptionLabels(name: String) {
+        var options: [String] = [name] + getNames(used: name, number: 3)
+    
+        option1Label.setTitle(options.remove(at: Int(arc4random_uniform(UInt32(options.count)))), for: UIControlState.normal)
+        option2Label.setTitle(options.remove(at: Int(arc4random_uniform(UInt32(options.count)))), for: UIControlState.normal)
+        option3Label.setTitle(options.remove(at: Int(arc4random_uniform(UInt32(options.count)))), for: UIControlState.normal)
+        option4Label.setTitle(options.remove(at: Int(arc4random_uniform(UInt32(options.count)))), for: UIControlState.normal)
+    }
+    
+    func outOfTime() {
+        submit(nil, btnNumber: 0, btn: nil)
+    }
+    
+    func handleTimer() {
+        timeLeft -= 1
+        timerLabel.text = "\(timeLeft)"
+        if timeLeft == 0 {
+            outOfTime()
+        }
+    }
+    
+    func setupGame() {
+        timeLeft = 5
+        timerLabel.text = "\(timeLeft)"
+        
+        option1Label.backgroundColor = UIColor.yellow
+        option2Label.backgroundColor = UIColor.yellow
+        option3Label.backgroundColor = UIColor.yellow
+        option4Label.backgroundColor = UIColor.yellow
+        
+        name = getName()
+        
+        var lower = name.lowercased()
+        
+        lower.remove(at: name.characters.index(of: " ")!)
+        personImage.image = UIImage(named: lower)
+        
+        setUpOptionLabels(name: name)
+        
+        //Handle time stuff
+        gameTimer = Timer.scheduledTimer(timeInterval:1.0, target: self, selector: #selector(handleTimer), userInfo: nil, repeats: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupGame()
     }
 
     override func didReceiveMemoryWarning() {
